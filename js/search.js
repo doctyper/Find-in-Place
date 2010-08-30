542cc41d0fb05cfdbde03fc4a8bbdcb6e278322d
Adam Luikart <me@adamluikart.com>
2010-08-30 17:05:35 -0500

    Refactoring search.js. Pulling needle definition out of search function, into passed-in parameter. Adding some exception-throwing for error/warning states. Adding a hint of documentation.

--- js/search.js
+++ js/search.js
@@ -1,49 +1,66 @@
 document.addEventListener("DOMContentLoaded", function () {
-	var d = document,
-		de = d.documentElement,
-		needle = de.getAttribute("data-search-term"),
-		needleLength = needle.length,
-		r = new RegExp(needle, "i"),
-		s = d.evaluate(
-			"//text()[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), '" + needle.toLowerCase() + "')]",
-			d.body,
-			null,
-			XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-			null
-		),
-		l = s.snapshotLength,
-		textNode, text, match, i, span, beginOffset, endOffset, parent;
-	
-	for (i = 0; i < l; i++) {
-		textNode = s.snapshotItem(i);
-		parent = textNode.parentNode;
+	(function search(needle) {
+		var d = document,
+			needleLength = needle.length;
 
-		while ((match = r.exec(textNode.data)) !== null) {
-			// console.log("Found " + match[0] + " at " + match.index + " in \"" + textNode.data + "\".");
+		if (needleLength < 1) {
+			throw("Error: Nice try. No searching for empty strings. Go stare into the void on your own dime.");
+		}
 
-			text = textNode.data;
-			beginOffset = match.index;
-			endOffset = beginOffset + needleLength;
-			
-			
-			if (text.length > endOffset) {
-				textNode.splitText(endOffset);
-			}
-			
-			if (beginOffset !== 0) {
-				textNode.splitText(beginOffset);
-				textNode = textNode.nextSibling;
-			}
-			
-			span = d.createElement("span");
-			parent.replaceChild(span, textNode);
-			span.appendChild(textNode);
-			span.className = "fip-result fip-inline-result";
-			
-			// console.log(span.nextSibling.data);
-			// console.log("================")
+		var caseInsensitiveNeedle = new RegExp(needle, "i"),
 
-			textNode = span.nextSibling;
-		}		
-	}
+			// XPath is write-only, like Regexes and Perl.
+			// This expression selects all text nodes that satisfy the condition that:
+			//		The node's text content contains the needle, lower-cased.
+			//		(XPath 2 has a lower-case() function, but iOS 4's Mobile Safari doesn't include it.)
+			exp = "//text()[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), '" + needle.toLowerCase() + "')]",
+
+			// Evaluate the XPath expression and get an ordered snapshot of the result nodes
+			snapshot = d.evaluate(
+				exp,
+				d.body,
+				null,
+				XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+				null),
+			l = snapshot.snapshotLength,
+			textNode, text, match, i, span, beginOffset, endOffset, parent;
+
+		if (l < 1) {
+			throw("Warning: Couldn't find the search term anywhere. Tried really hard.")
+		}
+
+		// check inside the resulting text nodes for as many matches as possible,
+		// wrapping each result in span.fip-result.fip-inline-result
+		for (i = 0; i < l; i++) {
+			textNode = snapshot.snapshotItem(i);
+			parent = textNode.parentNode;
+
+			while ((match = caseInsensitiveNeedle.exec(textNode.data)) !== null) {
+				// console.log("Found " + match[0] + " at " + match.index + " in \"" + textNode.data + "\".");
+
+				text = textNode.data;
+				beginOffset = match.index;
+				endOffset = beginOffset + needleLength;
+
+				if (text.length > endOffset) {
+					textNode.splitText(endOffset);
+				}
+
+				if (beginOffset !== 0) {
+					textNode.splitText(beginOffset);
+					textNode = textNode.nextSibling;
+				}
+
+				span = d.createElement("span");
+				parent.replaceChild(span, textNode);
+				span.appendChild(textNode);
+				span.className = "fip-result fip-inline-result";
+
+				// console.log(span.nextSibling.data);
+				// console.log("================")
+
+				textNode = span.nextSibling;
+			}		
+		}
+	}(document.documentElement.getAttribute("data-search-term")));
 }, false);
