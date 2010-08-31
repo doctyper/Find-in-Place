b6d33a9a5614d699122ebdcf8bf5892a9904ec04
Richard Herrera <rich@doctyper.com>
2010-08-31 13:50:41 -0700

    Removing old test.js file

--- resources/sprockets/src/test.js
+++
@@ -1,7 +0,0 @@
-//= require "namespace"
-//= require "scale"
-//= require "search"
-//= require "results"
-
-var term = document.documentElement.getAttribute("data-search-term");
-var search = new FIP.Search(term);
